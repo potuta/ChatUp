a5b301cd4a911cbf38a716033d507f46cc87fe0c
potuta <cyrilbautista2@gmail.com>
2024-06-27 13:09:07 +0800

    Changes assigned.

--- ChatUp/Server.Designer.cs
+++ ChatUp/Server.Designer.cs
@@ -29,29 +29,71 @@
         private void InitializeComponent()
         {
             richTextBox1 = new RichTextBox();
+            Start_Button = new Button();
+            textBox1 = new TextBox();
+            Send_Button = new Button();
             SuspendLayout();
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(307, 32);
+            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.Location = new Point(310, 34);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(454, 388);
+            richTextBox1.ReadOnly = true;
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.Vertical;
+            richTextBox1.Size = new Size(454, 337);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
+            richTextBox1.WordWrap = false;
             // 
-            // Form1
+            // Start_Button
+            // 
+            Start_Button.Location = new Point(82, 397);
+            Start_Button.Name = "Start_Button";
+            Start_Button.Size = new Size(75, 23);
+            Start_Button.TabIndex = 1;
+            Start_Button.Text = "Start Server";
+            Start_Button.UseVisualStyleBackColor = true;
+            Start_Button.Click += Start_Button_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(310, 397);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(361, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // Send_Button
+            // 
+            Send_Button.Location = new Point(689, 396);
+            Send_Button.Name = "Send_Button";
+            Send_Button.Size = new Size(75, 23);
+            Send_Button.TabIndex = 3;
+            Send_Button.Text = "Send";
+            Send_Button.UseVisualStyleBackColor = true;
+            Send_Button.Click += Send_Button_Click;
+            // 
+            // Server
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(Send_Button);
+            Controls.Add(textBox1);
+            Controls.Add(Start_Button);
             Controls.Add(richTextBox1);
-            Name = "Form1";
+            Name = "Server";
             Text = "Form1";
+            Load += Server_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private RichTextBox richTextBox1;
+        private static TextBox textBox1;
+        private static Button Send_Button;
+        private static Button Start_Button;
+        public static RichTextBox richTextBox1;
     }
 }
--- ChatUp/Server.cs
+++ ChatUp/Server.cs
@@ -1,10 +1,64 @@
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
 namespace ChatUp
 {
     public partial class Server : Form
     {
+        private static TcpListener server = new TcpListener(IPAddress.Any, 16969);
+        private static bool isServerStarted = false;
+
         public Server()
         {
             InitializeComponent();
+        }
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            while (isServerStarted == false)
+            {
+                richTextBox1.AppendText("\nStarting server...");
+                server.Start();
+                isServerStarted = true;
+                UpdateUI();
+                Thread.Sleep(1000);
+                richTextBox1.AppendText("\nWaiting for connection...");
+            }
+        }
+
+        private void ConnectClient()
+        {
+            TcpClient client = server.AcceptTcpClient();
+            int attempts = 0;
+            while (!client.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    client = server.AcceptTcpClient();
+                }
+                catch (SocketException e)
+                {
+                    MessageBox.Show("Connection attempts: " + attempts + "\n" + e.Message);
+                }
+            }
+            richTextBox1.AppendText("Client connected.");
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("Welcome administrator!");
+        }
+
+        private void UpdateUI()
+        {
+            richTextBox1.Update();
+        }
+
+        private void Send_Button_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
